9db6c0634ad2b8ad87c07cdfa3ec910564ce2f8e
无码 <dev@codeless.run>
2021-05-29 18:01:41 +0800

    线程池

--- main.go
+++ main.go
@@ -3,29 +3,57 @@ package main
 import (
 	"fmt"
 	"net"
-	"sync"
-	"time"
+	"sort"
 )
 
-func main() {
-	start := time.Now()
-	var wg sync.WaitGroup
-	for i := 21; i < 65535; i++ {
-		wg.Add(1)
-		go func(j int) {
-			defer wg.Done()
-			address := fmt.Sprintf("81.70.203.195:%d", j)
-			conn, err := net.Dial("tcp", address)
-			if err != nil {
-				fmt.Printf("端口: %s 已关闭\n", address)
-				return
-			}
-			_ = conn.Close()
-			fmt.Printf("端口: %s 已打开\n", address)
-		}(i)
+func worker(ports chan int, results chan int) {
+	for p := range ports {
+		address := fmt.Sprintf("81.70.203.195:%d", p)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			results <- 0
+			continue
+		}
+		_ = conn.Close()
+		results <- p
 	}
-	wg.Wait()
+}
 
-	elapsed := time.Since(start) / 1e9
-	fmt.Printf("\n\n%d seconds", elapsed)
+func main() {
+	ports := make(chan int, 100)
+	results := make(chan int)
+	var openports []int
+	var closeports []int
+
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
+	}
+
+	go func() {
+		// 分配工作
+		for i := 1; i < 1024; i++ {
+			ports <- i
+		}
+	}()
+
+	// 收集结果
+	for i := 1; i < 1024; i++ {
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		} else {
+			closeports = append(closeports, port)
+		}
+	}
+
+	close(ports)
+	close(results)
+
+	sort.Ints(openports)
+	sort.Ints(closeports)
+
+	fmt.Printf("打开的端口有: ")
+	for _, port := range openports {
+		fmt.Printf("%d ,", port)
+	}
 }
