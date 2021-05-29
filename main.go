cb674646f3ea372ac04c38b3cabed1b6965c52f3
无码 <dev@codeless.run>
2021-05-29 16:40:13 +0800

    使用异步的方式

--- main.go
+++ main.go
@@ -3,18 +3,29 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
+	"time"
 )
 
 func main() {
-
-	for i := 21; i < 30; i++ {
-		address := fmt.Sprintf("81.70.203.195:%d", i)
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			fmt.Printf("端口: %s 已关闭\n", address)
-			continue
-		}
-		_ = conn.Close()
-		fmt.Printf("端口: %s 已打开\n", address)
+	start := time.Now()
+	var wg sync.WaitGroup
+	for i := 21; i < 65535; i++ {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			address := fmt.Sprintf("81.70.203.195:%d", j)
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				fmt.Printf("端口: %s 已关闭\n", address)
+				return
+			}
+			_ = conn.Close()
+			fmt.Printf("端口: %s 已打开\n", address)
+		}(i)
 	}
+	wg.Wait()
+
+	elapsed := time.Since(start) / 1e9
+	fmt.Printf("\n\n%d seconds", elapsed)
 }
